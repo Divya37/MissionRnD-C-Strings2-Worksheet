c04c1bfb2c0ee3a36446dfcc63a705ceee6787b5
Divya37 <Divya37@missionrnd.com>
2016-01-15 08:49:22 +0500

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -15,7 +15,39 @@ int len(char *str)
 	for (i = 0; str[i] != '\0'; i++);
 	return i;
 }
+char *reverse(char *str)
+{
+	int i, j;
+	char temp;
+	for (i = 0, j = len(str) - 1; i <= len(str) / 2 && j >= len(str) / 2; i++, j--)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
+	return str;
+}
 char * get_last_word(char * str){
-	
-	return NULL;
+	int i, flag = 0, size = 0;
+	char *output;
+	if (str == NULL)
+		return NULL;
+	output = (char *)malloc(sizeof(char)*len(str));
+	for (i = len(str) - 1; i >= 0; i--)
+	{
+		if (str[i] == ' ')
+		{
+			if (flag == 1)
+				break;
+		}
+		else
+		{
+			output[size++] = str[i];
+			flag = 1;
+		}
+	}
+	output[size] = '\0';
+	output = reverse(output);
+	//printf("output:%s\n", output);
+	return output;
 }
